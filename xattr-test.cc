66040fc30db15c1ec19df46b3cdf22966d81478c
Bartek Kryza <bkryza@gmail.com>
2017-05-23 00:59:21 +0200

    First stable version

--- xattr-test.cc
+++ xattr-test.cc
@@ -3,11 +3,13 @@
 
 #include <cerrno>
 #include <exception>
+#include <fstream>
 #include <iostream>
 #include <map>
 #include <string>
 #include <sys/xattr.h>
 #include <utility>
+#include <regex>
 
 constexpr auto base64SampleImage =
     R"(R0lGODlhPQBEAPeoAJosM//AwO/AwHVYZ/z595kzAP/s7P+goOXMv8+fhw/v739/f+8PD98fH/8mJl+fn/9ZWb8/PzWlwv///6wWGbImAPgTEMImIN9gUFCEm/gDALULDN8PAD6atYdCTX9gUNKlj8wZAKUsAOzZz+UMAOsJAP/Z2ccMDA8PD/95eX5NWvsJCOVNQPtfX/8zM8+QePLl38MGBr8JCP+zs9myn/8GBqwpAP/GxgwJCPny78lzYLgjAJ8vAP9fX/+MjMUcAN8zM/9wcM8ZGcATEL+QePdZWf/29uc/P9cmJu9MTDImIN+/r7+/vz8/P8VNQGNugV8AAF9fX8swMNgTAFlDOICAgPNSUnNWSMQ5MBAQEJE3QPIGAM9AQMqGcG9vb6MhJsEdGM8vLx8fH98AANIWAMuQeL8fABkTEPPQ0OM5OSYdGFl5jo+Pj/+pqcsTE78wMFNGQLYmID4dGPvd3UBAQJmTkP+8vH9QUK+vr8ZWSHpzcJMmILdwcLOGcHRQUHxwcK9PT9DQ0O/v70w5MLypoG8wKOuwsP/g4P/Q0IcwKEswKMl8aJ9fX2xjdOtGRs/Pz+Dg4GImIP8gIH0sKEAwKKmTiKZ8aB/f39Wsl+LFt8dgUE9PT5x5aHBwcP+AgP+WltdgYMyZfyywz78AAAAAAAD///8AAP9mZv///wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAEAAKgALAAAAAA9AEQAAAj/AFEJHEiwoMGDCBMqXMiwocAbBww4nEhxoYkUpzJGrMixogkfGUNqlNixJEIDB0SqHGmyJSojM1bKZOmyop0gM3Oe2liTISKMOoPy7GnwY9CjIYcSRYm0aVKSLmE6nfq05QycVLPuhDrxBlCtYJUqNAq2bNWEBj6ZXRuyxZyDRtqwnXvkhACDV+euTeJm1Ki7A73qNWtFiF+/gA95Gly2CJLDhwEHMOUAAuOpLYDEgBxZ4GRTlC1fDnpkM+fOqD6DDj1aZpITp0dtGCDhr+fVuCu3zlg49ijaokTZTo27uG7Gjn2P+hI8+PDPERoUB318bWbfAJ5sUNFcuGRTYUqV/3ogfXp1rWlMc6awJjiAAd2fm4ogXjz56aypOoIde4OE5u/F9x199dlXnnGiHZWEYbGpsAEA3QXYnHwEFliKAgswgJ8LPeiUXGwedCAKABACCN+EA1pYIIYaFlcDhytd51sGAJbo3onOpajiihlO92KHGaUXGwWjUBChjSPiWJuOO/LYIm4v1tXfE6J4gCSJEZ7YgRYUNrkji9P55sF/ogxw5ZkSqIDaZBV6aSGYq/lGZplndkckZ98xoICbTcIJGQAZcNmdmUc210hs35nCyJ58fgmIKX5RQGOZowxaZwYA+JaoKQwswGijBV4C6SiTUmpphMspJx9unX4KaimjDv9aaXOEBteBqmuuxgEHoLX6Kqx+yXqqBANsgCtit4FWQAEkrNbpq7HSOmtwag5w57GrmlJBASEU18ADjUYb3ADTinIttsgSB1oJFfA63bduimuqKB1keqwUhoCSK374wbujvOSu4QG6UvxBRydcpKsav++Ca6G8A6Pr1x2kVMyHwsVxUALDq/krnrhPSOzXG1lUTIoffqGR7Goi2MAxbv6O2kEG56I7CSlRsEFKFVyovDJoIRTg7sugNRDGqCJzJgcKE0ywc0ELm6KBCCJo8DIPFeCWNGcyqNFE06ToAfV0HBRgxsvLThHn1oddQMrXj5DyAQgjEHSAJMWZwS3HPxT/QMbabI/iBCliMLEJKX2EEkomBAUCxRi42VDADxyTYDVogV+wSChqmKxEKCDAYFDFj4OmwbY7bDGdBhtrnTQYOigeChUmc1K3QTnAUfEgGFgAWt88hKA6aCRIXhxnQ1yg3BCayK44EWdkUQcBByEQChFXfCB776aQsG0BIlQgQgE8qO26X1h8cEUep8ngRBnOy74E9QgRgEAC8SvOfQkh7FDBDmS43PmGoIiKUUEGkMEC/PJHgxw0xH74yx/3XnaYRJgMB8obxQW6kL9QYEJ0FIFgByfIL7/IQAlvQwEpnAC7DtLNJCKUoO/w45c44GwCXiAFB/OXAATQryUxdN4LfFiwgjCNYg+kYMIEFkCKDs6PKAIJouyGWMS1FSKJOMRB/BoIxYJIUXFUxNwoIkEKPAgCBZSQHQ1A2EWDfDEUVLyADj5AChSIQW6gu10bE/JG2VnCZGfo4R4d0sdQoBAHhPjhIB94v/wRoRKQWGRHgrhGSQJxCS+0pCZbEhAAOw==)";
@@ -27,7 +29,21 @@ constexpr auto utf8LoremIpsum =
 static int __total_tests = 0;
 static int __failed_count = 0;
 
-template <typename... Ts> void LOG(Ts &&... args) {
+bool file_exists(const std::string &name) {
+  std::ifstream f(name.c_str());
+  return f.good();
+}
+
+std::string unquote_string(const std::string &s) {
+  if(s.size()>1 && s.front() == '\"' && s[s.size()-1] == '\"') {
+    return s.substr(1, s.size()-2);
+  }
+  else {
+    return s;
+  }
+}
+
+template <typename... Ts> void XTLOG(Ts &&... args) {
   int dummy[] = {0, ((std::cout << args), 0)...};
   static_cast<void>(dummy);
   std::cout << std::endl;
@@ -71,36 +87,31 @@ void EXPECT_XATTR_SET(std::string MSG, std::string F, std::string N,
   __total_tests++;
   try {
     set_xattr(F, N, V, 0);
+    auto test_value = unquote_string(V);
     auto xattr = get_xattr(F, N);
-    if (V != xattr) {
-      LOG(MSG, "...", "Failed -- ", V, " != ", xattr);
+
+    bool is_numeric = false;
+    try {
+      double numeric_xattr = std::stod(xattr);
+      is_numeric = true;
+      if(xattr == "NaN") 
+        is_numeric = false;
+    }
+    catch(std::exception &e) {
+    }
+    
+    if (is_numeric ? (std::stod(test_value) != std::stod(xattr)) : (test_value != xattr)) {
+      XTLOG(MSG, "...", "Failed -- ", "EXPECTED(", test_value, ") GOT(", xattr, ")");
       __failed_count++;
     } else {
-      LOG(MSG, "...", "OK");
+      XTLOG(MSG, "...", "OK");
     }
-  } catch (std::runtime_error &e) {
-    LOG(MSG, "...", "Failed -- ", e.what());
+  } catch (std::exception &e) {
+    XTLOG(MSG, "...", "Failed -- ", e.what());
     __failed_count++;
   }
 }
 
-void EXPECT_XATTR_SET_QUOTES(std::string MSG, std::string F, std::string N,
-                      std::string V) {
-  __total_tests++;
-  try {
-    set_xattr(F, N, V, 0);
-    auto xattr = std::string("\"")+get_xattr(F, N)+"\"";
-    if (V != xattr) {
-      LOG(MSG, "...", "Failed -- ", V, " != ", xattr);
-      __failed_count++;
-    } else {
-      LOG(MSG, "...", "OK");
-    }
-  } catch (std::runtime_error &e) {
-    LOG(MSG, "...", "Failed -- ", e.what());
-    __failed_count++;
-  }
-}
 
 void EXPECT_XATTR_NOTSET(std::string MSG, std::string F, std::string N,
                          std::string V) {
@@ -110,14 +121,14 @@ void EXPECT_XATTR_NOTSET(std::string MSG, std::string F, std::string N,
       set_xattr(F, N, V, 0);
       auto xattr = get_xattr(F, N);
       if (V != xattr) {
-        LOG(MSG, "...", "Failed -- ", V, " != ", xattr);
+        XTLOG(MSG, "...", "Failed -- ", "EXPECTED(", V, ") GOT(", xattr, ")");
         __failed_count++;
       } else {
-        LOG(MSG, "...", "Failed -- ", "Attribute set");
+        XTLOG(MSG, "...", "Failed -- ", "Attribute set");
         __failed_count++;
       }
     } catch (std::runtime_error &e) {
-      LOG(MSG, "...", "OK");
+      XTLOG(MSG, "...", "OK");
     }
   }
 }
@@ -127,11 +138,11 @@ void EXPECT_XATTR_NOTREPLACED(std::string MSG, std::string F, std::string N,
   __total_tests++;
   try {
     set_xattr(F, N, V, XATTR_REPLACE);
-    LOG(MSG, "...",
+    XTLOG(MSG, "...",
         "Failed -- Attribute should not be created if it didn't exist already");
     __failed_count++;
   } catch (std::runtime_error &e) {
-    LOG(MSG, "...", "OK");
+    XTLOG(MSG, "...", "OK");
   }
 }
 
@@ -140,18 +151,23 @@ void EXPECT_XATTR_NOTCREATED(std::string MSG, std::string F, std::string N,
   __total_tests++;
   try {
     set_xattr(F, N, V, XATTR_CREATE);
-    LOG(MSG, "...",
+    XTLOG(MSG, "...",
         "Failed -- Attribute should not be set if it existed already");
     __failed_count++;
   } catch (std::runtime_error &e) {
-    LOG(MSG, "...", "OK");
+    XTLOG(MSG, "...", "OK");
   }
 }
 
 int main(int argc, char **argv) {
 
   if (argc != 2) {
-    LOG("Usage:\n", argv[0], "EXISTING_TEST_FILE");
+    XTLOG("Usage:\n", argv[0], "EXISTING_TEST_FILE");
+    exit(-1);
+  }
+
+  if(!file_exists(argv[1])) {
+    XTLOG("File ", argv[1], " does not exist!");
     exit(-1);
   }
 
@@ -197,11 +213,14 @@ int main(int argc, char **argv) {
   EXPECT_XATTR_SET("Setting attribute with just double quotes",
                     argv[1], "valueInDoubleQoutes", R"("text")");
 
-  // EXPECT_XATTR_SET("Setting attribute with Json object",
-  //                  argv[1], "jsonObject", R"({"key":1})");
+  EXPECT_XATTR_SET("Setting attribute with complex name",
+                    argv[1], R"(%^&*()(*&^%$#$%^&*(/)", "123");
 
-  // EXPECT_XATTR_SET("Setting attribute with Json array",
-  //                  argv[1], "jsonObject", R"([{"key1":1},{"key2":2}])");
+  EXPECT_XATTR_SET("Setting attribute with Json object",
+                   argv[1], "jsonObject", R"({"key":1})");
+
+  EXPECT_XATTR_SET("Setting attribute with Json array",
+                   argv[1], "jsonObject2", R"([{"key1":1},{"key2":2}])");
 
 
   for(int i = 0; i<100; i++) {
@@ -236,18 +255,32 @@ int main(int argc, char **argv) {
   EXPECT_XATTR_NOTREPLACED("Non existing attributed should not be replaced with XATTR_REPLACE",
                            argv[1], "TO_BE_CREATED", "stringValue");
 
+  //
   // Test Big List of Naughty Strings
-  // int i = 0;
-  // for(auto& naughtyString : big_list_of_naughty_strings) {
-  // EXPECT_XATTR_SET("Setting naughty string <<<"+naughtyString+">>> value should work", argv[1],
-  //                  "naughtyString_"+std::to_string(i++), naughtyString);
-  // }
+  //  https://github.com/minimaxir/big-list-of-naughty-strings
+  //
+  int i = 0;
+  for(auto& naughtyString : big_list_of_naughty_strings) {
+    if(!naughtyString.empty()
+      && naughtyString.length() < 127
+      && naughtyString != "￾"
+      && !std::all_of(naughtyString.begin(),naughtyString.end(),isspace)) {
+      EXPECT_XATTR_SET("Setting naughty string <<<"+naughtyString+">>> name should work", argv[1],
+                     naughtyString, "naughtyString_"+std::to_string(i));
+    }
+    else {
+      EXPECT_XATTR_NOTSET("Setting naughty string <<<"+naughtyString+">>> name should work", argv[1],
+                     naughtyString, "naughtyString_"+std::to_string(i));
+    }
+    EXPECT_XATTR_SET("Setting naughty string <<<"+naughtyString+">>> value should work", argv[1],
+                   "naughtyString_"+std::to_string(i++), naughtyString);
+  }
 
   if(__failed_count) {
-    std::cout << __failed_count << " of " << __total_tests << " FAILED" << std::endl;
+    XTLOG(__failed_count, " of ", __total_tests, " FAILED");
   }
   else {
-     std::cout << "All " << __total_tests << " PASSED" << std::endl;
+     XTLOG("All ", __total_tests, " PASSED");
   }
 
   return __failed_count > 0;
