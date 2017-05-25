09052548336306bce1ecad7f903ad1b0793d3142
Bartek Kryza <bkryza@gmail.com>
2017-05-25 15:34:43 +0200

    Fixed key size condition

--- xattr-test.cc
+++ xattr-test.cc
@@ -6,10 +6,10 @@
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <regex>
 #include <string>
 #include <sys/xattr.h>
 #include <utility>
-#include <regex>
 
 constexpr auto base64SampleImage =
     R"(R0lGODlhPQBEAPeoAJosM//AwO/AwHVYZ/z595kzAP/s7P+goOXMv8+fhw/v739/f+8PD98fH/8mJl+fn/9ZWb8/PzWlwv///6wWGbImAPgTEMImIN9gUFCEm/gDALULDN8PAD6atYdCTX9gUNKlj8wZAKUsAOzZz+UMAOsJAP/Z2ccMDA8PD/95eX5NWvsJCOVNQPtfX/8zM8+QePLl38MGBr8JCP+zs9myn/8GBqwpAP/GxgwJCPny78lzYLgjAJ8vAP9fX/+MjMUcAN8zM/9wcM8ZGcATEL+QePdZWf/29uc/P9cmJu9MTDImIN+/r7+/vz8/P8VNQGNugV8AAF9fX8swMNgTAFlDOICAgPNSUnNWSMQ5MBAQEJE3QPIGAM9AQMqGcG9vb6MhJsEdGM8vLx8fH98AANIWAMuQeL8fABkTEPPQ0OM5OSYdGFl5jo+Pj/+pqcsTE78wMFNGQLYmID4dGPvd3UBAQJmTkP+8vH9QUK+vr8ZWSHpzcJMmILdwcLOGcHRQUHxwcK9PT9DQ0O/v70w5MLypoG8wKOuwsP/g4P/Q0IcwKEswKMl8aJ9fX2xjdOtGRs/Pz+Dg4GImIP8gIH0sKEAwKKmTiKZ8aB/f39Wsl+LFt8dgUE9PT5x5aHBwcP+AgP+WltdgYMyZfyywz78AAAAAAAD///8AAP9mZv///wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAEAAKgALAAAAAA9AEQAAAj/AFEJHEiwoMGDCBMqXMiwocAbBww4nEhxoYkUpzJGrMixogkfGUNqlNixJEIDB0SqHGmyJSojM1bKZOmyop0gM3Oe2liTISKMOoPy7GnwY9CjIYcSRYm0aVKSLmE6nfq05QycVLPuhDrxBlCtYJUqNAq2bNWEBj6ZXRuyxZyDRtqwnXvkhACDV+euTeJm1Ki7A73qNWtFiF+/gA95Gly2CJLDhwEHMOUAAuOpLYDEgBxZ4GRTlC1fDnpkM+fOqD6DDj1aZpITp0dtGCDhr+fVuCu3zlg49ijaokTZTo27uG7Gjn2P+hI8+PDPERoUB318bWbfAJ5sUNFcuGRTYUqV/3ogfXp1rWlMc6awJjiAAd2fm4ogXjz56aypOoIde4OE5u/F9x199dlXnnGiHZWEYbGpsAEA3QXYnHwEFliKAgswgJ8LPeiUXGwedCAKABACCN+EA1pYIIYaFlcDhytd51sGAJbo3onOpajiihlO92KHGaUXGwWjUBChjSPiWJuOO/LYIm4v1tXfE6J4gCSJEZ7YgRYUNrkji9P55sF/ogxw5ZkSqIDaZBV6aSGYq/lGZplndkckZ98xoICbTcIJGQAZcNmdmUc210hs35nCyJ58fgmIKX5RQGOZowxaZwYA+JaoKQwswGijBV4C6SiTUmpphMspJx9unX4KaimjDv9aaXOEBteBqmuuxgEHoLX6Kqx+yXqqBANsgCtit4FWQAEkrNbpq7HSOmtwag5w57GrmlJBASEU18ADjUYb3ADTinIttsgSB1oJFfA63bduimuqKB1keqwUhoCSK374wbujvOSu4QG6UvxBRydcpKsav++Ca6G8A6Pr1x2kVMyHwsVxUALDq/krnrhPSOzXG1lUTIoffqGR7Goi2MAxbv6O2kEG56I7CSlRsEFKFVyovDJoIRTg7sugNRDGqCJzJgcKE0ywc0ELm6KBCCJo8DIPFeCWNGcyqNFE06ToAfV0HBRgxsvLThHn1oddQMrXj5DyAQgjEHSAJMWZwS3HPxT/QMbabI/iBCliMLEJKX2EEkomBAUCxRi42VDADxyTYDVogV+wSChqmKxEKCDAYFDFj4OmwbY7bDGdBhtrnTQYOigeChUmc1K3QTnAUfEgGFgAWt88hKA6aCRIXhxnQ1yg3BCayK44EWdkUQcBByEQChFXfCB776aQsG0BIlQgQgE8qO26X1h8cEUep8ngRBnOy74E9QgRgEAC8SvOfQkh7FDBDmS43PmGoIiKUUEGkMEC/PJHgxw0xH74yx/3XnaYRJgMB8obxQW6kL9QYEJ0FIFgByfIL7/IQAlvQwEpnAC7DtLNJCKUoO/w45c44GwCXiAFB/OXAATQryUxdN4LfFiwgjCNYg+kYMIEFkCKDs6PKAIJouyGWMS1FSKJOMRB/BoIxYJIUXFUxNwoIkEKPAgCBZSQHQ1A2EWDfDEUVLyADj5AChSIQW6gu10bE/JG2VnCZGfo4R4d0sdQoBAHhPjhIB94v/wRoRKQWGRHgrhGSQJxCS+0pCZbEhAAOw==)";
@@ -35,10 +35,9 @@ bool file_exists(const std::string &name) {
 }
 
 std::string unquote_string(const std::string &s) {
-  if(s.size()>1 && s.front() == '\"' && s[s.size()-1] == '\"') {
-    return s.substr(1, s.size()-2);
-  }
-  else {
+  if (s.size() > 1 && s.front() == '\"' && s[s.size() - 1] == '\"') {
+    return s.substr(1, s.size() - 2);
+  } else {
     return s;
   }
 }
@@ -94,14 +93,15 @@ void EXPECT_XATTR_SET(std::string MSG, std::string F, std::string N,
     try {
       double numeric_xattr = std::stod(xattr);
       is_numeric = true;
-      if(xattr == "NaN") 
+      if (xattr == "NaN")
         is_numeric = false;
+    } catch (std::exception &e) {
     }
-    catch(std::exception &e) {
-    }
-    
-    if (is_numeric ? (std::stod(test_value) != std::stod(xattr)) : (test_value != xattr)) {
-      XTLOG(MSG, "...", "Failed -- ", "EXPECTED(", test_value, ") GOT(", xattr, ")");
+
+    if (is_numeric ? (std::stod(test_value) != std::stod(xattr))
+                   : (test_value != xattr)) {
+      XTLOG(MSG, "...", "Failed -- ", "EXPECTED(", test_value, ") GOT(", xattr,
+            ")");
       __failed_count++;
     } else {
       XTLOG(MSG, "...", "OK");
@@ -261,16 +261,17 @@ int main(int argc, char **argv) {
   //
   int i = 0;
   for(auto& naughtyString : big_list_of_naughty_strings) {
-    if(!naughtyString.empty()
-      && naughtyString.length() < 127
-      && naughtyString != "￾"
-      && !std::all_of(naughtyString.begin(),naughtyString.end(),isspace)) {
-      EXPECT_XATTR_SET("Setting naughty string <<<"+naughtyString+">>> name should work", argv[1],
-                     naughtyString, "naughtyString_"+std::to_string(i));
-    }
-    else {
-      EXPECT_XATTR_NOTSET("Setting naughty string <<<"+naughtyString+">>> name should work", argv[1],
-                     naughtyString, "naughtyString_"+std::to_string(i));
+    if(naughtyString.length() < 127) {
+      if(!naughtyString.empty()
+          && naughtyString != "￾"
+          && !std::all_of(naughtyString.begin(),naughtyString.end(),isspace)) {
+        EXPECT_XATTR_SET("Setting naughty string <<<"+naughtyString+">>> name should work", argv[1],
+                       naughtyString, "naughtyString_"+std::to_string(i));
+      }
+      else {
+        EXPECT_XATTR_NOTSET("Setting naughty string <<<"+naughtyString+">>> name should work", argv[1],
+                       naughtyString, "naughtyString_"+std::to_string(i));
+      }
     }
     EXPECT_XATTR_SET("Setting naughty string <<<"+naughtyString+">>> value should work", argv[1],
                    "naughtyString_"+std::to_string(i++), naughtyString);
